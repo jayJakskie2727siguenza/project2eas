ca4770ac6f01e6053381d828e0a72fa1dbe95432
Jayson <jayJakskie2727siguenza@gmail.com>
2021-07-08 15:09:51 +0800

    updating

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,35 +1,41 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import '../styles/style.scss'
+import "../styles/style.scss"
 import Layout from "../components/layouts/layout"
-import MainFooter from '../components/layouts/Mainfooter'
+import MainFooter from "../components/layouts/Mainfooter"
 import SEO from "../components/seo"
 
+import {
+  Banner,
+  Features,
+  Status,
+  ProvideSolution,
+  Teams,
+} from "../components/aboutCompany/aboutcompany"
 
-import { Banner, Features, Status, ProvideSolution, Teams } from '../components/aboutCompany/aboutcompany'
-
-const data = graphql`{
+const data = graphql`
+  {
     wpgraph2eas {
       pageBy(uri: "about") {
         page_seo_acf {
-        siteMetaTitle
-        siteMetaDescription
-      }
-       title(format: RENDERED)
+          siteMetaTitle
+          siteMetaDescription
+        }
+        title(format: RENDERED)
       }
     }
-  }`
+  }
+`
 
 const AboutCompany = () => {
-  const { wpgraph2eas: {
-    pageBy: {
-      page_seo_acf: {
-        siteMetaTitle,
-        siteMetaDescription
+  const {
+    wpgraph2eas: {
+      pageBy: {
+        page_seo_acf: { siteMetaTitle, siteMetaDescription },
+        title,
       },
-      title
-    }
-  } } = useStaticQuery(data)
+    },
+  } = useStaticQuery(data)
   return (
     <Layout>
       <SEO title={siteMetaTitle} description={siteMetaDescription} />
